431356453b6d3b24f8112a6375380b3f233b80b1
Eddie Monge <eddie@eddiemonge.com>
2015-08-04 18:37:36 -0700

    various updates
     * Clean up some code styling
     * More logging
     * Change update interval to be one second beyond an hour
     * Updated response to be slimmer and what is being used for site
     * Hopefully better promise handling and fetching
     * Uses longer of GH or NPM description
     * Dont choke if GH doesn't respond
     * Dont require GH response

--- index.js
+++ index.js
@@ -1,129 +1,127 @@
-var _ = require('lodash');
+'use strict';
+
 var Q = require('q');
 var npmKeyword = require('npm-keyword');
 var packageJson = require('package-json');
 var ghGot = require('gh-got');
 var EventEmitter = require('events').EventEmitter;
 
-var UPDATE_INTERVAL_IN_SECONDS = 60 * 60;
+var UPDATE_INTERVAL_IN_SECONDS = 60 * 60 + 1;
 
 
 function createComponentData(npm, gh) {
+  gh = gh || {
+    description: '',
+    owner: {
+      login: '',
+      html_url: '',
+    },
+    html_url: '',
+    stargazers_count: '',
+    watchers_count: ''
+  };
+
+  var description = npm.description && gh.description ? (
+    npm.description.length > gh.description.length ?
+      npm.description : gh.description
+    ) : npm.description || gh.description || '';
+
   return {
     name: npm.name,
-    description: npm.description || gh.description,
+    description: description,
+    stars: gh.stargazers_count || gh.watchers_count,
+    website: npm.website || gh.html_url,
     owner: npm.author && npm.author.name || gh.owner.login,
     ownerWebsite: npm.author && npm.author.url || gh.owner.html_url,
-    website: gh.html_url,
-    stars: gh.stargazers_count,
-    created: gh.created_at,
-    updated: gh.updated_at
   };
 }
 
 function condenseMeta(pkg) {
   return {
     name: pkg.name,
-    author: pkg.author ? pkg.author : '',
-    gitURL: pkg.repository ? pkg.repository.url : '',
+    description: pkg.description,
+    author: pkg.author,
+    website: pkg.homepage,
+    repo: pkg.repository && pkg.repository.type === 'git' ?
+      pkg.repository.url : false
+  };
+}
+
+function promiseCallback (promise, transform) {
+  return function (err, data) {
+    if (err) {
+      promise.reject(err);
+      return;
+    }
+    promise.resolve(transform ? transform(data) : data);
   };
 }
 
 function getNpmPackageList() {
-  // TODO: there has to be a better way to combine cb interface with promises than this mess
+  console.log('Querying for keywords');
   var d = Q.defer();
-  var keyword = process.env.NPM_LIST_KEYWORD || 'yeoman-generator';
-
-  npmKeyword(keyword, function (err, packages) {
-    if (err) {
-      d.reject(err);
-      return;
-    }
-
-    d.resolve(packages);
-  });
-
+  npmKeyword(
+    process.env.NPM_LIST_KEYWORD || 'yeoman-generator',
+    promiseCallback(d)
+  );
   return d.promise;
 }
 
 function getNpmPackages(list) {
+  console.log('Getting ' + list.length + ' packages');
   return Q.all(list.map(function (el) {
     var d = Q.defer();
-
-    packageJson(el.name, function (err, pkg) {
-      if (err) {
-        d.reject(err);
-        return;
-      }
-
-      d.resolve(condenseMeta(pkg));
-    });
-
+    packageJson(
+      el.name,
+      promiseCallback(d, condenseMeta)
+    );
     return d.promise;
   }));
 }
 
 function getGithubStats(list) {
-  // make sure we have a git URL
-  return Q.all(list.map(function (el) {
+  console.log('Getting GH stats');
+  return Q.allSettled(list.map(function (el) {
     var d = Q.defer();
-    var re = /github\.com\/([\w\-\.]+)\/([\w\-\.]+)/i;
 
-    var parsedUrl;
-    if (el.gitURL) {
-      parsedUrl = re.exec(el.gitURL && el.gitURL.replace(/\.git$/, ''));
-    }
+    if (el.repo) {
+      var re = /github\.com\/([\w\-\.]+)\/([\w\-\.]+)/i;
+      var parsedUrl = re.exec(el.repo && el.repo.replace(/\.git$/, ''));
 
-    // Only return components from github
-    if (!parsedUrl) {
-      d.resolve();
-      return d.promise;
-    }
-
-    var user = parsedUrl[1];
-    var repo = parsedUrl[2];
-
-    ghGot('repos/' + user + '/' + repo, {
-      query: {
-        client_id: process.env.GITHUB_CLIENT_ID,
-        client_secret: process.env.GITHUB_CLIENT_SECRET
-      },
-      headers: {
-        'user-agent': 'https://github.com/yeoman/yeoman-generator-list'
-      }
-    }, function (err, data, res) {
-      if (err) {
-        if (err.code === 404) {
-          // don't fail just because the repo doesnt exist
-          // instead just return `undefined` and filter it out later
-          d.resolve();
-        } else {
-          d.reject(new Error('GitHub fetch failed\n' + err + '\n' + data));
-        }
+      if (parsedUrl) {
+        el.repo = parsedUrl[1] + '/' + parsedUrl[2];
       } else {
-        d.resolve(createComponentData(el, data));
+        el.repo = false;
       }
-    });
+    }
+
+    if (!el.repo) {
+      d.resolve(createComponentData(el));
+    } else {
+      ghGot('repos/' + el.repo, {
+        query: {
+          client_id: process.env.GITHUB_CLIENT_ID,
+          client_secret: process.env.GITHUB_CLIENT_SECRET
+        },
+        headers: {
+          'user-agent': 'https://github.com/yeoman/yeoman-generator-list'
+        }
+      }, function (err, data, res) {
+        if (err) {
+          if (err.code === 404 || err.code === 403) {
+            d.resolve(createComponentData(el));
+          } else {
+            d.reject(new Error('GitHub fetch failed\n' + err + '\n' + data));
+          }
+        } else {
+          d.resolve(createComponentData(el, data));
+        }
+      });
+    }
 
     return d.promise;
   }));
 }
-
-function filterInvalidValues(list) {
-  // TODO: this is pretty ugly. find a better way.
-  // filter out null values introduced in previous steps
-  return Q.all(_.reject(list, function (val) {
-    return val == null;
-  }));
-}
-
-function fetchPluginList() {
-  return Q.fcall(getNpmPackageList)
-    .then(getNpmPackages)
-    .then(getGithubStats)
-    .then(filterInvalidValues);
-}
-
 
 module.exports = {
   pluginList: null,
@@ -134,16 +132,24 @@ module.exports = {
   },
 
   update: function () {
-    console.log('Updating the plugin list');
-    fetchPluginList()
+    console.log('Updating the list');
+    Q.fcall(getNpmPackageList)
+      .then(getNpmPackages)
+      .then(getGithubStats)
       .then(function (pluginList) {
-        console.log('plugin list updated');
-        this.pluginList = pluginList;
-        this.hub.emit('update', pluginList);
+        console.log('Processing ' + pluginList.length + ' plugins');
+        this.pluginList = [];
+        pluginList.forEach(function(plugin) {
+          if (plugin.state === 'fulfilled') {
+            this.pluginList.push(plugin.value);
+          }
+        }.bind(this));
+        console.log('List updated: ' + this.pluginList.length + ' results');
+        this.hub.emit('update', this.pluginList);
         setTimeout(this.update.bind(this), UPDATE_INTERVAL_IN_SECONDS * 1000);
       }.bind(this))
       .catch(function (err) {
-        console.log('Error while fetching plugin list', err);
+        console.log('Error while fetching list', err);
       });
   },
 
@@ -154,10 +160,10 @@ module.exports = {
       d.resolve(this.pluginList);
     } else {
       this.hub.on('update', function (pluginList) {
-        d.resolve(pluginList)
+        d.resolve(pluginList);
       });
     }
 
-    return d.promise
+    return d.promise;
   }
 };
