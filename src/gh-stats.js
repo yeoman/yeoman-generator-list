43173f844c9170c0163837cee4c760f070d4d73c
Eddie Monge <eddie@eddiemonge.com>
2015-12-10 16:56:59 -0800

    add debounce for gh fetching

--- src/gh-stats.js
+++ src/gh-stats.js
@@ -12,8 +12,9 @@ module.exports = (list, limit) => {
   log.info('GHStats: Fetching stats for %s packages', limit);
 
   let count = 0;
+  let timer = 1;
 
-  return Promise.all(list.map(pkg => {
+  return Promise.all(list.map(pkg => new Promise(resolve => {
     if (pkg.repo) {
       let url = gh(pkg.repo);
 
@@ -25,48 +26,59 @@ module.exports = (list, limit) => {
     }
 
     if (!pkg.repo) {
-      return merge(pkg);
+      resolve(merge(pkg));
+      return;
     }
 
     if (--limit < 1) {
       if (limit === 0) {
         log.info('GHStats: Limit reached for GH API calls');
       }
-      return merge(pkg);
+      resolve(merge(pkg));
+      return;
     }
 
-    count++;
-    return ghGot('repos/' + pkg.repo, {
-      query: {
-        client_id: process.env.GITHUB_CLIENT_ID,
-        client_secret: process.env.GITHUB_CLIENT_SECRET
-      },
-      headers: {
-        'user-agent': 'https://github.com/yeoman/yeoman-generator-list'
-      }
-    })
-    .then(res => merge(pkg, res.body))
-    .catch(err => {
-      log.warn(
-        'GHStats: Could not fetch stats for %s package because %s (url: %s)',
-        pkg.name,
-        (err && (err.message || err.statusCode)) || err,
-        pkg.repo
-      );
-      count--;
-      return merge(pkg);
-    });
-  }))
+    setTimeout(() => {
+      count++;
+      ghGot('repos/' + pkg.repo, {
+        query: {
+          client_id: process.env.GITHUB_CLIENT_ID,
+          client_secret: process.env.GITHUB_CLIENT_SECRET
+        },
+        headers: {
+          'user-agent': 'https://github.com/yeoman/yeoman-generator-list'
+        }
+      })
+      .then(res => resolve(merge(pkg, res.body)))
+      .catch(err => {
+        err = err || {};
+        log.warn(
+          'GHStats: Could not fetch stats for %s package because %s (url: %s)',
+          pkg.name,
+          err.message || err.statusCode || err,
+          pkg.repo
+        );
+        count--;
+
+        if (err.statusCode === 404) {
+          resolve(false);
+          return
+        }
+
+        resolve(merge(pkg));
+      });
+    }, timer++ * 50);
+  })))
   .then(pkgs => {
     let message = 'GHStats: Fetched stats for %s packages';
     message += '. Skipped %s packages';
     log.info(message, count, list.length - count);
 
     let total = pkgs.length;
-    pkgs = pkgs.filter(pkg => Boolean(pkg.description.trim()));
+    pkgs = pkgs.filter(pkg => pkg && Boolean(pkg.description.trim()));
     let invalid = pkgs.length - total;
     if (invalid > 0) {
-      log.info('GHStats: Filtered out %s bad description packages', invalid);
+      log.info('GHStats: Filtered out %s bad packages', invalid);
     }
 
     return pkgs;
